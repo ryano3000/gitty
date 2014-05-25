3ac559827b647f49c40f936d56aea2bd591eb191
ryan perera <conversationtheory@gmail.com>
2014-05-25 18:55:13 +0100

    Initial Commit

---
+++ Git Test/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Git Test
+//
+//  Created by Ryan on 25/05/2014.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Ryan on 25/05/2014.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
